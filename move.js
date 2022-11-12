4bc6398e87184769b1fb99734fd4949d1f6ce452
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-11-12 11:46:46 -0800

    use newer page synchronizer. support sections for individual project

--- move.js
+++ move.js
@@ -10,17 +10,26 @@ function addHandlers() {
             moveTo(id.slice(0, hyphen));
         });
     });
+
+    elems = document.querySelectorAll(".project");
+    elems.forEach((c) => {
+        let id = c.id;
+        let elem = document.getElementById(id);
+        if (!elem) {return;}
+        elem.addEventListener("click", () => {
+            let hyphen = id.indexOf("-");
+            moveTo(id);
+        });
+    });
 }
 
 function moveTo(id) {
     let elem = document.getElementById(id);
     if (!elem) {return;}
-
     let content = document.body;
-
     let offsetTop = elem.offsetTop;
-
     content.scrollTop = offsetTop;
+    window.location.hash = id;
 }
 
 window.onload = addHandlers;
